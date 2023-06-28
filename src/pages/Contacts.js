cc57ef92c0a22fb57bf06b022467ace0c92813ef
Enard <enard@Enards-MBP.modem>
2023-06-28 21:38:47 +1000

    create column title

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,10 +1,29 @@
 import Header from "../components/Header";
+import SubTitle from "../components/ui/SubTitle";
+import { Colors } from "../const/Colors";
 import "./styles.css";
 
 function Contacts() {
+  const columnTitles = [
+    "Name",
+    "Username",
+    "Email",
+    "Phone",
+    "City",
+    "Company",
+  ];
   return (
     <div id="container">
       <Header />
+      <div className="column-title-parent">
+        {columnTitles.map((title, idx) => {
+          return (
+            <div key={idx} className="column-title-wrapper">
+              <SubTitle>{title}</SubTitle>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
